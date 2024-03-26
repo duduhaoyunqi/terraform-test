e3bc0593a5c3b7a0bf8018b21bc1eeeeb3b46385
duduhaoyunqi <88752377+duduhaoyunqi@users.noreply.github.com>
2024-03-26 16:06:58 +0800

    Update test1.tf
    updated

--- test1.tf
+++ test1.tf
@@ -34,7 +34,7 @@ resource "aws_iam_user" "iam_users" {
 #   value = [for k,v in aws_iam_user.iam_users: v.name]
 # }
 
-# for v in aws_iam_user.iam_users : v
+# for v in var.map : v
 # output "name5-1" {
 #   value = [for user in aws_iam_user.iam_users : user]
 # }
