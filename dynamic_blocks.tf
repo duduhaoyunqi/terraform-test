24e84c6bb833ce3b4f344c31b1c85b9c27c4c388
duduhaoyunqi <88752377+duduhaoyunqi@users.noreply.github.com>
2024-03-28 10:59:36 +0800

    Create dynamic_blocks.tf

---
+++ dynamic_blocks.tf
@@ -0,0 +1,111 @@
+# variables.tf
+variable "ingress_rules" {
+  type = map(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = {
+    ssh = {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    http = {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
+
+# security_group.tf
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Allow inbound traffic"
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+}
+
+output "ingress_rules_map" {
+  value = var.ingress_rules
+}
+#   + ingress_rules_map       = {
+#       + http = {
+#           + cidr_blocks = [
+#               + "0.0.0.0/0",
+#             ]
+#           + from_port   = 80
+#           + protocol    = "tcp"
+#           + to_port     = 80
+#         }
+#       + ssh  = {
+#           + cidr_blocks = [
+#               + "0.0.0.0/0",
+#             ]
+#           + from_port   = 22
+#           + protocol    = "tcp"
+#           + to_port     = 22
+#         }
+#     }
+
+output "ingress_from_ports" {
+  value = {
+    for key, rule in var.ingress_rules:
+      key => rule.from_port
+  }
+}
+#   + ingress_from_ports      = {
+#       + http = 80
+#       + ssh  = 22
+#     }
+
+output "values_ingress_rules" {
+  value = values(var.ingress_rules)
+}
+#   + values_ingress_rules    = [
+#       + {
+#           + cidr_blocks = [
+#               + "0.0.0.0/0",
+#             ]
+#           + from_port   = 80
+#           + protocol    = "tcp"
+#           + to_port     = 80
+#         },
+#       + {
+#           + cidr_blocks = [
+#               + "0.0.0.0/0",
+#             ]
+#           + from_port   = 22
+#           + protocol    = "tcp"
+#           + to_port     = 22
+#         },
+#     ]
+
+output "ingress_from_ports_list" {
+  value = [
+    for rule in values(var.ingress_rules) : rule.from_port
+  ]
+}
+#   + ingress_from_ports_list = [
+#       + 80,
+#       + 22,
+#     ]
+
+
+
+
+
+
+  
